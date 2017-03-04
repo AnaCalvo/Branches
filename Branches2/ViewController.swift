5923bbe0b23a6110c92d7e00d27364c7716943af
Ana Calvo <anacber@gmail.com>
2017-03-04 18:06:45 +0100

    Function name changed to downloadPersons()

--- Branches2/ViewController.swift
+++ Branches2/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let personDownloader = Downloader()
-        personDownloader.downloadJSON()
+        personDownloader.downloadPersons()
         
     }
 
