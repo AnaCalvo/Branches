575e0c02414295c09a065ca8c889fa37bd7058ee
Ana Calvo <anacber@gmail.com>
2017-03-04 16:50:36 +0100

    This version includes the Person Model

---
+++ Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  Branches2
+//
+//  Created by Ana Calvo on 4/3/17.
+//  Copyright © 2017 Ana Calvo. All rights reserved.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let surname: String
+    let gender: String
+    let region: String
+}
+
